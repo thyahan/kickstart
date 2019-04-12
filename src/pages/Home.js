1af57f8676227e94d6f3f0e75bdff68aa82681cf
Yutthana Yahan <thyahan@gmail.com>
2019-04-13 01:39:18 +0700

    implement hooks useReducer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useReducer, useEffect } from 'react';
 import Header from '../components/Header';
 import Landing from '../components/Landing';
 import Footer from '../components/Footer';
 import withWrap from '../hocs/withWrap';
+import { initial, reducer } from '../reducers/user';
 
 const Home = () => {
+  const [{ id, username }, dispatch] = useReducer(reducer, initial);
+
+  useEffect(() => {
+    dispatch({
+      type: 'FETCH_USER_SUCCESS',
+      payload: {
+        id: 21,
+        username: 'meitoey'
+      }
+    });
+  }, []);
+
   return (
     <div className="wrap-home">
       <Header />
+      <span>{`${id}: ${username}`}</span>
       <Landing />
       <Footer />
     </div>
